9575292b00c4e75cc7af35d77f0a4988abe2c7eb
けん <wasd0109@Kens-MacBook-Air.local>
2021-06-30 16:31:54 +0900

    Extract config variables to separate file

---
+++ functions/utils/config.js
@@ -0,0 +1,11 @@
+exports.firebaseConfig = {
+  apiKey: 'AIzaSyBquX_3YbrzQglkREmdMWANs5SvpRzt-Js',
+  authDomain: 'idolgame-back-f095d.firebaseapp.com',
+  databaseURL:
+    'https://idolgame-back-f095d-default-rtdb.asia-southeast1.firebasedatabase.app',
+  projectId: 'idolgame-back-f095d',
+  storageBucket: 'idolgame-back-f095d.appspot.com',
+  messagingSenderId: '1019697904525',
+  appId: '1:1019697904525:web:90a224dd5f317c6d679bc4',
+  measurementId: 'G-XP3P64M2RS',
+};
